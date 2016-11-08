14445f009390751fc594e5364a4566361d018231
haanh764 <ngohaanh764@gmail.com>
2016-11-08 13:17:58 +0700

    first commit

---
+++ application/views/backend/footer.php
@@ -0,0 +1,7 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; 2016 <strong>PhpSoftwares.com School Manager</strong>. 
+    Developed by 
+	<a href="http://phpsoftwares.com" 
+    	target="_blank">Php Softwares</a>
+</footer>
